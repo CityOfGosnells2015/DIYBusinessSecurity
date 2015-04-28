847a272222c35c34e288ef227bdc60121a268eef
james112358 <041401076@P-B223-168794.student.central>
2015-04-28 09:54:25 +0800

    Changed To load from welcom activity

--- App/CityOfGosnellsDIYBusinessSecurity/app/src/main/java/com/mylayouts/jm/cityofgosnellsdiybusinesssecurity/WelcomeActivity.java
+++ App/CityOfGosnellsDIYBusinessSecurity/app/src/main/java/com/mylayouts/jm/cityofgosnellsdiybusinesssecurity/WelcomeActivity.java
@@ -2,12 +2,24 @@ package com.mylayouts.jm.cityofgosnellsdiybusinesssecurity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 /**
@@ -26,6 +38,11 @@ public class WelcomeActivity extends ActionBarActivity implements View.OnClickLi
 
     Button firstTimeClick;
     SharedPreferences prefs;
+    /*
+    URL Of Checklist Data to be retrieved.
+    */
+    private final String CHECKLIST_URL = "http://www.gosnells.wa.gov.au/feed.rss?listname=Security%20Audit%20Checklist";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +54,11 @@ public class WelcomeActivity extends ActionBarActivity implements View.OnClickLi
         //Initialise Button and add listener
         firstTimeClick = (Button) findViewById(R.id.btnFirstClick);
         firstTimeClick.setOnClickListener(this);
+        firstTimeClick.setEnabled(false);
+
+        //Execute Async Task
+        DownloadJSONTask task = new DownloadJSONTask();
+        task.execute(new String[]{CHECKLIST_URL});
     }
 
 
@@ -95,5 +117,92 @@ public class WelcomeActivity extends ActionBarActivity implements View.OnClickLi
 
         }
 
+    }
+
+
+    /**
+     *  James McNeil
+     *
+     *  DownloadJSONTask uses the AsyncTask class
+     *  Gets the JSON String from the url
+     */
+    private class DownloadJSONTask extends AsyncTask<String, Void, String> {
+
+        /**
+         *
+         * @param urls
+         * @return
+         */
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            for (String url : urls) {
+
+                /*
+                    Add Code to fetch JSON String from URL Provided by the city of Gosnells
+                 */
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet(url);
+                try {
+
+
+                    HttpResponse execute = client.execute(httpGet);
+                    InputStream content = execute.getEntity().getContent();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    StringBuilder sBuilder = new StringBuilder();
+
+                    String line = null;
+                    while ((line = buffer.readLine()) != null) {
+                        sBuilder.append(line + "\n");
+                    }
+
+                    /*
+                        Close Stream + Get String
+                     */
+                    content.close();
+                    response = sBuilder.toString();
+
+
+                } catch (ClientProtocolException e) {
+                    Log.e("ClientProtocolException", e.getMessage());
+                } catch (IOException e) {
+                    Log.e("IOException", e.getMessage());
+                }
+
+
+            }
+            Log.i("Response", "\n\n" + response);
+
+            /*
+
+             */
+            return response;
+        }
+
+        /*
+         After JSON String is retrived Wait and then move to Welcome Activity
+
+         !!! Data handling needs to be added here
+         */
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+                /*
+                    Splash Screen will always be visible for at least 3 seconds
+                    sleep( time in milliseconds)
+                 */
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Log.e("Interrupted", "" + e.getMessage());
+            }
+
+            /*
+                Set Button to Enabled
+             */
+            firstTimeClick.setEnabled(true);
+
+        }
     }
 }
