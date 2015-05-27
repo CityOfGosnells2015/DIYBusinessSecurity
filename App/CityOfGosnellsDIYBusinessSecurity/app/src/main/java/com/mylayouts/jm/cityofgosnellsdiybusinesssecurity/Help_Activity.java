5bc31959863b48fde2e885fc00c77937e50fa355
Gustavo Tavares Dias <gustavotavaresdias@gmail.com>
2015-05-27 16:24:41 +0800

    Help activity - unfinished
    Help activity - unfinished

--- App/CityOfGosnellsDIYBusinessSecurity/app/src/main/java/com/mylayouts/jm/cityofgosnellsdiybusinesssecurity/Help_Activity.java
+++ App/CityOfGosnellsDIYBusinessSecurity/app/src/main/java/com/mylayouts/jm/cityofgosnellsdiybusinesssecurity/Help_Activity.java
@@ -2,16 +2,31 @@ package com.mylayouts.jm.cityofgosnellsdiybusinesssecurity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 
 public class Help_Activity extends ActionBarActivity {
 
     SharedPreferences prefs;
     int themeValue;
+    TableLayout table;
+    TableRow tr;
+    HashMap emergencyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +44,8 @@ public class Help_Activity extends ActionBarActivity {
 
         //Set layout for activity
         setContentView(R.layout.activity_help_);
+
+        populateTable();
     }
 
 
@@ -61,5 +78,69 @@ public class Help_Activity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void populateTable(){
+
+        table = (TableLayout) findViewById(R.id.main_table);
+        table.setColumnStretchable(0,true);
+        table.setColumnStretchable(1,true);
+        tr = new TableRow(this);
+
+        //Set head line on the table
+        TextView txtName = new TextView(this);
+        txtName.setText("Name"); // set the text for the header
+        txtName.setTextColor(Color.BLACK); // set the color
+        txtName.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr.addView(txtName); // add the column to the table row here
+
+        TextView txtPhone = new TextView(this);
+        txtPhone.setText("Phone"); // set the text for the header
+        txtPhone.setTextColor(Color.BLACK); // set the color
+        txtPhone.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr.addView(txtPhone); // add the column to the table row here
+
+        TextView txtWebSite = new TextView(this);
+        txtWebSite.setText("WebSite"); // set the text for the header
+        txtWebSite.setTextColor(Color.BLACK); // set the color
+        txtWebSite.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr.addView(txtWebSite); // add the column to the table row here
+
+        TextView txtEdit = new TextView(this);
+        txtEdit.setText("Edit"); // set the text for the header
+        txtEdit.setTextColor(Color.BLACK); // set the color
+        txtEdit.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr.addView(txtEdit); // add the column to the table row here
+
+        table.addView(tr);
+        GlobalChecklist globalChecklist= (GlobalChecklist) getApplication();
+
+        emergencyList = globalChecklist.getEmergencyContacts();
+
+        if (!emergencyList.isEmpty()){
+            for(int num=0; num < emergencyList.size(); num++) {
+                TableRow row = new TableRow(this);
+
+                //Set column name
+                TextView colName = new TextView(this);
+                colName.setText(emergencyList.get(num).toString());
+                row.addView(colName);
+
+                //Set column phone
+                ImageView colPhone = new ImageView(this);
+                colPhone.setImageResource(R.drawable.ic_call);
+                final String number = emergencyList.get(num).toString();
+                colPhone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View arg0) {
+                        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + number));
+                        startActivity(intent);
+                    }
+                });
+                row.addView(colPhone);
+
+                table.addView(row);
+            }
+        }
     }
 }
