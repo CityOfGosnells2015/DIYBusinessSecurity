25c8e073100255ee3bdf2a5ffc212c4a9cdf3401
Gustavo Tavares Dias <gustavotavaresdias@gmail.com>
2016-04-04 16:44:36 +0800

    new major

--- App/CityOfGosnellsDIYBusinessSecurity/app/src/main/java/com/mylayouts/jm/cityofgosnellsdiybusinesssecurity/NotificationAdapter.java
+++ App/CityOfGosnellsDIYBusinessSecurity/app/src/main/java/com/mylayouts/jm/cityofgosnellsdiybusinesssecurity/NotificationAdapter.java
@@ -1,6 +1,7 @@
 package com.mylayouts.jm.cityofgosnellsdiybusinesssecurity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +18,15 @@ public class NotificationAdapter extends ArrayAdapter<Notification>{
 
     private final Activity context;
     private final ArrayList<Notification> list;
+    private SharedPreferences prefs;
 
     public NotificationAdapter(Activity context, ArrayList<Notification> list) {
         super(context,R.layout.activity_display_notification, list);
 
         this.list = list;
         this.context = context;
+        //Preference file
+        prefs = context.getSharedPreferences("Preferences", context.MODE_PRIVATE);
     }
 
     static class ViewHolder {
@@ -42,9 +46,11 @@ public class NotificationAdapter extends ArrayAdapter<Notification>{
             viewHolder.checkbox  = (CheckBox) view.findViewById(R.id.check);
             viewHolder.checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
+                //Action when selected an option
                 public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
                     Notification element = (Notification) viewHolder.checkbox.getTag();
                     element.setSelected(buttonView.isChecked());
+                    prefs.edit().putBoolean(element.getId(), true).commit();
                 }
             });
             view.setTag(viewHolder);
@@ -54,9 +60,13 @@ public class NotificationAdapter extends ArrayAdapter<Notification>{
             ((ViewHolder) view.getTag()).checkbox.setTag(list.get(position));
         }
 
+        //Loading preference values
+        list.get(position).setSelected(prefs.getBoolean(list.get(position).getId(), false));
+
         ViewHolder holder = (ViewHolder) view.getTag();
         holder.checkbox.setText(list.get(position).getQuestion());
         holder.checkbox.setChecked(list.get(position).isSelected());
+        //Toast.makeText(context, list.get(position).getId(), Toast.LENGTH_SHORT).show();
 
         return view;
     }
